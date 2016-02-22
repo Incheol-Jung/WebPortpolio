13ee25dcc1ab73594e66f87037d8f9a86ba8f33e
sungjinCho <luxdolorosa@naver.com>
2016-02-22 18:12:33 +0900

    dsa

--- WebPortpolioApiSvr/src/main/java/org/jij/WebPortpolio/api/TestController.java
+++ WebPortpolioApiSvr/src/main/java/org/jij/WebPortpolio/api/TestController.java
@@ -12,16 +12,17 @@
 package org.jij.WebPortpolio.api;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.jij.WebPortpolio.model.Country;
 import org.jij.WebPortpolio.model.JsonResponse;
 import org.jij.WebPortpolio.model.Test;
-import org.jij.WebPortpolio.service.CountryService;
 import org.jij.WebPortpolio.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,19 +38,65 @@ public class TestController {
 		logger.debug("Create TestController");		
 	}
 	
-	@RequestMapping(value = "/Test", method = RequestMethod.GET)//@PathVariable
-	public @ResponseBody JsonResponse getCountryList() {	
-		logger.debug("/TestController classed.");
+	@RequestMapping(value = "/TestCreate/{testName}/{testAge}/{testActive}")
+	public @ResponseBody JsonResponse createTest(
+			@PathVariable("testName") String testName,
+			@PathVariable("testAge") int testAge,
+			@PathVariable("testActive") String testActive
+			) {	
+		JsonResponse response = new JsonResponse();
+		response.setSuccess(true);	
+		System.out.println("[sjc]>>>>>>[" + testName+"]");
+		System.out.println("[sjc]>>>>>>[" + testAge+"]");
+		System.out.println("[sjc]>>>>>>[" + testActive+"]");
+		return response;
+	}
+	
+	@RequestMapping(value = "/TestRead")
+	public @ResponseBody JsonResponse createReadAll() {	
+		logger.debug("/createReadAll classed.");
 		
 		JsonResponse response = new JsonResponse();
 		response.setSuccess(true);		
 		List<Test> testList = new ArrayList<Test>();
+		Map<String, Object> paramMap = new HashMap<String, Object>();
 		try{			
-			testList = testService.getTestList();			
+			testList = testService.getTestListAll();			
 		}catch(Exception e){
 			System.out.println("[sjc]>>>>>>[" + e.toString()+"]");
 		}
 		response.setData(testList);
 		return response;
-	} 
+	}
+	
+	
+	@RequestMapping(value = "/TestRead/{testId}")
+	public @ResponseBody JsonResponse createReadDetail(
+			@PathVariable("testId") int testId
+			) {	
+		logger.debug("/createReadDetail classed.");
+		
+		JsonResponse response = new JsonResponse();
+		response.setSuccess(true);		
+		List<Test> testList = new ArrayList<Test>();
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		try{			
+			paramMap.put("testId", testId);
+			testList = testService.getTestListDetail(paramMap);			
+		}catch(Exception e){
+			System.out.println("[sjc]>>>>>>[" + e.toString()+"]");
+		}
+		response.setData(testList);
+		return response;
+	}
+	
+	@RequestMapping(value = "/TestUpdate")
+	public void createUpdate() {	
+		
+	}
+	
+	@RequestMapping(value = "/TestDelete")
+	public void createDelete() {	
+		
+	}
 }
--- WebPortpolioApiSvr/src/main/java/org/jij/WebPortpolio/service/TestService.java
+++ WebPortpolioApiSvr/src/main/java/org/jij/WebPortpolio/service/TestService.java
@@ -12,7 +12,9 @@
 package org.jij.WebPortpolio.service;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
@@ -20,6 +22,8 @@ import org.jij.WebPortpolio.model.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+
 
 @Service
 public class TestService {
@@ -30,12 +34,31 @@ public class TestService {
 	
 	@SuppressWarnings("unchecked")
 	@Transactional
-	public List<Test> getTestList(){
+	public List<Test> getTestListAll(){
 		logger.debug("getTestList");	
-		Query qry = sessionFactory.getCurrentSession().getNamedQuery("test.getTestList");
+		Query qry = sessionFactory
+				.getCurrentSession()
+				.getNamedQuery("test.getTestListALL");
 				
 		List<Test> list = qry.list();
 		
 		return list;
 	}
+	
+	@SuppressWarnings("unchecked")
+	@Transactional
+	public List<Test> getTestListDetail(Map<String, Object> paramMap){
+		logger.debug("getTestList");	
+		System.out.println("[sjc]>>>>>getTestList>[" + NumberUtils.createInteger(paramMap.get("testId").toString())+"]");
+		
+		Query qry = sessionFactory
+				.getCurrentSession()
+				.getNamedQuery("test.getTestListDetail")
+				.setInteger("testId", NumberUtils.createInteger(paramMap.get("testId").toString()));
+		List<Test> list = qry.list();
+		
+		return list;
+	}
+	
+	
 }
